c0910944d0dcf68749731a89baffd3e5fb268c85
g-gundam <g-gundam@users.noreply.github.com>
2025-08-30 10:33:28 -0700

    Move comment block to bottom

--- notify.clj
+++ notify.clj
@@ -23,13 +23,13 @@
           (apply shell)
           ))))
 
+;; Read one EDN expression from `*in*` and use it as arguments to `notify`.
+(apply notify (edn/read *in*))
+
 (comment
   ;; dunst uses icons from /usr/share/icons/gnome/32x32/status by default.
   (notify "Just the message, please.")
   (notify "Custom Title" "Body of Notification")
   (notify {"-a" "Test" "--urgency" "critical"} "Happening" "...until it does.")
-  (notify {"-a" "Test" "--urgency" "low"} "Boredom" "nothing ever happens...")
-  )
+  (notify {"-a" "Test" "--urgency" "low"} "Boredom" "nothing ever happens..."))
 
-;; Read one EDN expression from `*in*` and use it as arguments to `notify`.
-(apply notify (edn/read *in*))
